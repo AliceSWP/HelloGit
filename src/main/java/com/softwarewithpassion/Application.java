85ebad6507bf0e3eede8bfcd6b2c3c8f21eed93a
Alice <alice@softwarewithpassion.com>
2020-01-19 08:30:51 -0800

    Greetings from Alice first!

--- src/main/java/com/softwarewithpassion/Application.java
+++ src/main/java/com/softwarewithpassion/Application.java
@@ -3,6 +3,7 @@ package com.softwarewithpassion;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("Hello Git! I am Alice.");
         System.out.println("Hello Git! I am Bob.");
     }
 }
