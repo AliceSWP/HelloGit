cb542c19625fdcba48c4e9e8a9058f49b6ccebd4
Bob <bob@softwarewithpassion.com>
2020-01-19 08:25:11 -0800

    Greetings from Bob

--- src/main/java/com/softwarewithpassion/Application.java
+++ src/main/java/com/softwarewithpassion/Application.java
@@ -3,6 +3,6 @@ package com.softwarewithpassion;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git! I am Alice.");
+        System.out.println("Hello Git! I am Bob.");
     }
 }
