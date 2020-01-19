8e79bddb4c094c1e65c7c99d4cb5d75ca592912f
Bob <bob@softwarewithpassion.com>
2020-01-19 08:39:00 -0800

    Greetings refactoring

--- src/main/java/com/softwarewithpassion/Application.java
+++ src/main/java/com/softwarewithpassion/Application.java
@@ -3,6 +3,10 @@ package com.softwarewithpassion;
 public class Application {
 
     public static void main(String[] args) {
+        showGreetings();
+    }
+
+    private static void showGreetings() {
         System.out.println("Hello Git! I am Bob.");
     }
 }
